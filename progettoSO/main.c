96148b88bd05cb73760c474d2359a57a41e88b00
Filippo <filippozaccari@gmail.com>
2021-08-12 12:41:04 +0200

    le pipe si mandano 1 messaggio

--- progettoSO/main.c
+++ progettoSO/main.c
@@ -1,37 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<unistd.h>
-#include<sys/stat.h>
-#include<fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define PIPE "P1"
 
 
+int fd;
+
+int openPipe() {
+    int fd;
+
+    do {  //Keep trying to open the file until successful
+        fd = open (PIPE, O_WRONLY); //Open named pipe for writing
+            if (fd == -1) sleep (1); // Try again in 1 second
+    } while (fd == -1);
+
+    return fd;
+}
 
 
-void createPipe(char *pathName) {
-    mknod(pathName, O_WRONLY, 0);
-    //pipe();
+void sendToPipe(char *message) {
+    write (fd, message, strlen(message) + 1); //Write message down pipe
 }
 
 int main()
 {
-    char *path;
-    path = "/home/filippo/Scrivania/progettoSO/dataset2.csv";
+    char *path = "/home/filippo/Scrivania/progettoSO/dataset2.csv";
     char car;
     FILE *fp;
-    printf("prova");
-
     int dimRiga = 0;
 
     fp = fopen(path, "r"); // Apertura del file in sola lettura
+    fd = openPipe();
 
     // Scarto la prima riga leggendola a vuoto
     while(car != '\n') { // Legge la prima riga
         car = fgetc(fp); // Leggendo carattere per carattere
-        printf("%c", car);
+        //printf("%c", car);
     }
 
-    car = 'a';
-    printf("\n");
+    car = 'a'; // Reset del carattere di controllo
+    //printf("\n");
 
     // Conta dimensioni righe
     while(car != '\n') {
@@ -39,22 +51,28 @@ int main()
         dimRiga++; // Conta di quanti caratteri è lunga la seconda riga, si assume che le righe abbiano la stessa lunghezza
     }
 
-    printf("dimRiga: %d\n", dimRiga);
+    //printf("dimRiga: %d\n", dimRiga);
 
     fseek(fp, -dimRiga, 1); // Mi riposiziono all'inizio della seconda riga
     char buffer[dimRiga];
 
-
+    fgets(buffer, dimRiga, fp);
+   // strcat(buffer, "\0");
+    sendToPipe(buffer);
+    printf("%s\n\n\n", buffer);
+/*
    while(!feof(fp)) { // Scorro tutto il file
         sleep(1);
         fgets(buffer, dimRiga, fp);
-       // sendToPipe(buffer);
+        sendToPipe(buffer);
        // sendToSocket(buffer);
        // sendToSharedFile(buffer);
         printf("%s\n\n\n", buffer);
     }
-
+*/
+    close(fd);
     fclose(fp);
+    printf("FINE");
     return 0;
 }
 
