29280135108f8e5ee657dadc13281916d5e0e4d5
Filippo <filippozaccari@gmail.com>
2021-08-18 12:47:27 +0200

    manca da fare l'avviamento del programma

---
+++ progettoSO/constHeader.h
@@ -0,0 +1,15 @@
+#ifndef CONSTHEADER_H_INCLUDED
+#define CONSTHEADER_H_INCLUDED
+
+#define MODEXEC 1
+#define PATHDATASET "../dataset2.csv"
+#define FILEPATH "./fileP3"
+#define PIDPATH "filePid"
+#define OUTPUT "voted_output"
+#define SYSLOG "system_log"
+#define DEFAULT_PROTOCOL 0
+#define SOCKET "socketP2"
+#define SOCKETDF "socketDF"
+#define PIPE "pipeP1"
+
+#endif // CONSTHEADER_H_INCLUDED
